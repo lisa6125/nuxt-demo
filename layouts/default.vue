e2a948868233b949b2d0d5eb38cd9ddcc8641f98
lisa6125 <fxck6125@gmail.com>
2021-08-07 18:04:45 +0800

    nuxt初始認識(router)

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header />
+    <nuxt-link to="/">首頁</nuxt-link>
+    <nuxt-link to="/about">about</nuxt-link>
+      <p>------這邊是layout，裡面是router-view-----</p>
+      <Nuxt />
+      <p>---------------------------------------</p>
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>我是首頁</p>
+</template>
+
+<script>
+export default {
+  layout:'default',
+}
+</script>
+
+<style>
+
+</style>
