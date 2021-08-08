ae46236dd796bf7273cce185df8b5c4bda8e633e
lisa6125 <fxck6125@gmail.com>
2021-08-08 19:51:02 +0800

    asyncData照片輪播實作
    利用 async await 跟 axios 取得照片

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="container">
     <p>我是首頁</p>
-    <p>我的名字是 {{ name }} </p>
-    <button @click="change()">改名字</button>
+    <p>照片輪播</p>
+    <img :src="res[pic].url" alt="">
+    <button @click="change(-1)">上一張</button>
+    <button @click="change(1)">下一張</button>
   </div>
 
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name:'index',
   layout:'default',
-  asyncData(){
-    const name = 'mike';
-    return {name};
+  async asyncData(){
+    const res = await axios.get('https://vue-lessons-api.herokuapp.com/photo/list')
+    return {res:res.data};
   },
   data(){
     return {
-      name:'',
+      res:[],
+      pic:0,
     }
   },
   methods:{
-    change(){
-      this.name = 'tom不是mike'
+    change(num){
+      if(this.pic + num === -1){
+        this.pic = this.res.length-1
+      }else{
+        this.pic = (this.pic + num ) % this.res.length
+      }
     }
   }
 }
