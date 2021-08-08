34682d1b51f34ecb0c0f9c8b79335ddb8eb258c1
lisa6125 <fxck6125@gmail.com>
2021-08-08 19:19:34 +0800

    asyncData伺服器渲染換面

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,30 @@
 <template>
-  <p>我是首頁</p>
+  <div class="container">
+    <p>我是首頁</p>
+    <p>我的名字是 {{ name }} </p>
+    <button @click="change()">改名字</button>
+  </div>
+
 </template>
 
 <script>
 export default {
+  name:'index',
   layout:'default',
+  asyncData(){
+    const name = 'mike';
+    return {name};
+  },
+  data(){
+    return {
+      name:'',
+    }
+  },
+  methods:{
+    change(){
+      this.name = 'tom不是mike'
+    }
+  }
 }
 </script>
 
